08d48b6fe5a0a94c01fa75acd965051bbf4e4d76
Andrei Drexler <makro@rq3.com>
2019-11-26 18:46:42 +0200

    Removed extraneous NUL character

--- src/demo/material.h
+++ src/demo/material.h
@@ -35,7 +35,7 @@ namespace Demo {
 
 		constexpr u32 Version = Hash(
 			#define PP_DEMO_HASH_MATERIAL_NAME(path, shader, texture, contents, draw, light)		path "*" #texture "*" #contents "*" #draw "*"
-			DEMO_MATERIALS(PP_DEMO_HASH_MATERIAL_NAME) "\0"
+			DEMO_MATERIALS(PP_DEMO_HASH_MATERIAL_NAME)
 			#undef PP_DEMO_HASH_MATERIAL_NAME
 		);
 
