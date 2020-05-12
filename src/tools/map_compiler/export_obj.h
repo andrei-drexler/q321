27e0b4c5f52a57b3430d37b704376658e6f58011
Andrei Drexler <makro@rq3.com>
2020-05-12 19:08:50 +0300

    Removed left-over code.

--- src/tools/map_compiler/export_obj.h
+++ src/tools/map_compiler/export_obj.h
@@ -1,69 +1,5 @@
 #pragma once
 
-const float
-PI = Math::PI,
-PI_2 = PI * 0.5f;
-
-float ApproxAtan(float z)
-{
-    const float n1 = 0.97239411f;
-    const float n2 = -0.19194795f;
-    return (n1 + n2 * z * z) * z;
-}
-
-float ApproxAtan2(float y, float x)
-{
-    if (x != 0.0f)
-    {
-        if (fabsf(x) > fabsf(y))
-        {
-            const float z = y / x;
-            if (x > 0.0)
-            {
-                // atan2(y,x) = atan(y/x) if x > 0
-                return ApproxAtan(z);
-            }
-            else if (y >= 0.0)
-            {
-                // atan2(y,x) = atan(y/x) + PI if x < 0, y >= 0
-                return ApproxAtan(z) + PI;
-            }
-            else
-            {
-                // atan2(y,x) = atan(y/x) - PI if x < 0, y < 0
-                return ApproxAtan(z) - PI;
-            }
-        }
-        else // Use property atan(y/x) = PI/2 - atan(x/y) if |y/x| > 1.
-        {
-            const float z = x / y;
-            if (y > 0.0)
-            {
-                // atan2(y,x) = PI/2 - atan(x/y) if |y/x| > 1, y > 0
-                return -ApproxAtan(z) + PI_2;
-            }
-            else
-            {
-                // atan2(y,x) = -PI/2 - atan(x/y) if |y/x| > 1, y < 0
-                return -ApproxAtan(z) - PI_2;
-            }
-        }
-    }
-    else
-    {
-        if (y > 0.0f) // x = 0, y > 0
-        {
-            return PI_2;
-        }
-        else if (y < 0.0f) // x = 0, y < 0
-        {
-            return -PI_2;
-        }
-    }
-    return 0.0f; // x,y = 0. Could return NaN instead.
-}
-
-
 bool ExportObj(const Q3::Map& map, const char* path) {
 	if (map.entities.empty())
 		return false;
@@ -118,7 +54,6 @@ bool ExportObj(const Q3::Map& map, const char* path) {
 			auto get_edge_angle = [&] (u8 idx) {
 				auto& edge = edges[idx];
 				vec3 delta = (edge.first_point + edge.second_point) * 0.5f - center;
-				return ApproxAtan2(dot(delta, y_axis), dot(delta, x_axis));
 				return atan2f(dot(delta, y_axis), dot(delta, x_axis));
 			};
 
