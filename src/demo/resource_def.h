e12a8f6c8bcc6d54c970b3a63afb5f23cfa6c41e
Andrei Drexler <makro@rq3.com>
2020-06-24 02:15:06 +0300

    Added sfx/flame1side (-16B).

--- src/demo/resource_def.h
+++ src/demo/resource_def.h
@@ -272,6 +272,7 @@
 	x("sfx/beam",								beam,			White,			NonSolid,	Translucent|NeedsUV,	(0))\
 	x("sfx/flame1_hell",						flame,			_128x256,		NonSolid,	Translucent|NeedsUV,	(600,8,6,1))\
 	x("sfx/flame2",								flame,			_128x256,		NonSolid,	Translucent|NeedsUV,	(5500,8,6,1))\
+	x("sfx/flame1side",							flame,			_128x256,		NonSolid,	Translucent|NeedsUV,	(0))\
 	x("common/weapclip",						Generic,		White,			Solid,		Invisible,				(0))\
 	x("common/clip",							Generic,		White,			Solid,		Invisible,				(0))\
 	x("common/nodraw",							Generic,		White,			Solid,		Invisible,				(0))\
