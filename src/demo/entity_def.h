2ba36b14726c25f57b391ce2acfcd7195206641d
Andrei Drexler <makro@rq3.com>
2020-09-18 01:49:24 +0300

    Removed superfluous namespace.

--- src/demo/entity_def.h
+++ src/demo/entity_def.h
@@ -20,37 +20,37 @@
 ////////////////////////////////////////////////////////////////
 
 #define DEMO_ENTITY_TYPES_WEAPONS(x)\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(weapon_gauntlet,				"Gauntlet",				Demo::Model::gauntlet_barrel,		  0,   0,   0)\
-	x(weapon_machinegun,			"Machinegun",			Demo::Model::machinegun_barrel,		  0,   0,   0)\
-	x(weapon_shotgun,				"Shotgun",				Demo::Model::shotgun,				  0,   0,   0)\
-	x(weapon_rocketlauncher,		"Rocket launcher",		Demo::Model::rocketl,				  0,   0,   0)\
-	x(weapon_railgun,				"Railgun",				Demo::Model::railgun,				  0,   0,   0)\
-	x(weapon_plasmagun,				"Plasma Gun",			Demo::Model::plasma,				  0,   0,   0)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(weapon_gauntlet,				"Gauntlet",				Model::gauntlet_barrel,		  0,   0,   0)\
+	x(weapon_machinegun,			"Machinegun",			Model::machinegun_barrel,	  0,   0,   0)\
+	x(weapon_shotgun,				"Shotgun",				Model::shotgun,				  0,   0,   0)\
+	x(weapon_rocketlauncher,		"Rocket launcher",		Model::rocketl,				  0,   0,   0)\
+	x(weapon_railgun,				"Railgun",				Model::railgun,				  0,   0,   0)\
+	x(weapon_plasmagun,				"Plasma Gun",			Model::plasma,				  0,   0,   0)\
 
 #define DEMO_ENTITY_TYPES_AMMO(x)\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(ammo_bullets,					"Bullets",				Demo::Model::rocketam,				128, 112,   2)\
-	x(ammo_rockets,					"Rockets",				Demo::Model::rocketam,				128,   2,   2)\
-	x(ammo_shells,					"Shells",				Demo::Model::rocketam,				128,  64,   2)\
-	x(ammo_slugs,					"Slugs",				Demo::Model::rocketam,				  0,  80,  32)\
-	x(ammo_cells,					"Cells",				Demo::Model::rocketam,				112,   0,  64)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(ammo_bullets,					"Bullets",				Model::rocketam,			128, 112,   2)\
+	x(ammo_rockets,					"Rockets",				Model::rocketam,			128,   2,   2)\
+	x(ammo_shells,					"Shells",				Model::rocketam,			128,  64,   2)\
+	x(ammo_slugs,					"Slugs",				Model::rocketam,			  0,  80,  32)\
+	x(ammo_cells,					"Cells",				Model::rocketam,			112,   0,  64)\
 
 #define DEMO_ENTITY_TYPES_HEALTH(x)\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(item_health,					"25 Health",			Demo::Model::large_cross,			160, 128,  48)\
-	x(item_health_large,			"50 Health",			Demo::Model::large_cross,			144,  88,  56)\
-	x(item_health_mega,				"Mega Health",			Demo::Model::mega_cross,			  8, 119, 234)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(item_health,					"25 Health",			Model::large_cross,			160, 128,  48)\
+	x(item_health_large,			"50 Health",			Model::large_cross,			144,  88,  56)\
+	x(item_health_mega,				"Mega Health",			Model::mega_cross,			  8, 119, 234)\
 
 #define DEMO_ENTITY_TYPES_ARMOR(x)\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(item_armor_combat,			"Armor",				Demo::Model::armor_red,				255, 255,   0)\
-	x(item_armor_shard,				"Armor Shard",			Demo::Model::shard,					 80,  88,  86)\
-	x(item_armor_body,				"Heavy Armor",			Demo::Model::armor_red,				255,   0,  16)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(item_armor_combat,			"Armor",				Model::armor_red,			255, 255,   0)\
+	x(item_armor_shard,				"Armor Shard",			Model::shard,				 80,  88,  86)\
+	x(item_armor_body,				"Heavy Armor",			Model::armor_red,			255,   0,  16)\
 
 #define DEMO_ENTITY_TYPES_POWERUPS(x)\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(item_quad,					"Quad Damage",			Demo::Model::quad,					  8, 119, 234)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(item_quad,					"Quad Damage",			Model::quad,				  8, 119, 234)\
 
 #define DEMO_ENTITY_TYPES_PICKUPS(x)\
 	DEMO_ENTITY_TYPES_WEAPONS(x)\
@@ -60,21 +60,21 @@
 	DEMO_ENTITY_TYPES_POWERUPS(x)\
 
 #define DEMO_ENTITY_TYPES(x)		\
-	/*Name							Description				Model								  R,   G,   B*/\
-	x(worldspawn,					"",						-1,									  0,   0,   0)\
-	x(trigger_teleport,				"",						-1,									  0,   0,   0)\
-	x(trigger_multiple,				"",						-1,									  0,   0,   0)\
-	x(trigger_hurt,					"",						-1,									  0,   0,   0)\
-	x(trigger_push,					"",						-1,									  0,   0,   0)\
-	/*x(light,						"",						-1,									  0,   0,   0)*/\
-	x(target_position,				"",						-1,									  0,   0,   0)\
-	/*x(target_speaker,				"",						-1,									  0,   0,   0)*/\
-	x(misc_model,					"",						-1,									  0,   0,   0)\
-	x(info_player_deathmatch,		"",						-1,									  0,   0,   0)\
-	x(info_player_intermission,		"",						-1,									  0,   0,   0)\
-	x(misc_teleporter_dest,			"",						-1,									  0,   0,   0)\
+	/*Name							Description				Model						  R,   G,   B*/\
+	x(worldspawn,					"",						-1,							  0,   0,   0)\
+	x(trigger_teleport,				"",						-1,							  0,   0,   0)\
+	x(trigger_multiple,				"",						-1,							  0,   0,   0)\
+	x(trigger_hurt,					"",						-1,							  0,   0,   0)\
+	x(trigger_push,					"",						-1,							  0,   0,   0)\
+	/*x(light,						"",						-1,							  0,   0,   0)*/\
+	x(target_position,				"",						-1,							  0,   0,   0)\
+	/*x(target_speaker,				"",						-1,							  0,   0,   0)*/\
+	x(misc_model,					"",						-1,							  0,   0,   0)\
+	x(info_player_deathmatch,		"",						-1,							  0,   0,   0)\
+	x(info_player_intermission,		"",						-1,							  0,   0,   0)\
+	x(misc_teleporter_dest,			"",						-1,							  0,   0,   0)\
 	DEMO_ENTITY_TYPES_PICKUPS(x)\
-	x(target_remove_powerups,		"",						-1,									  0,   0,   0)\
+	x(target_remove_powerups,		"",						-1,							  0,   0,   0)\
 
 ////////////////////////////////////////////////////////////////
 
