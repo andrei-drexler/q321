9fedd4a769668aec23a86bc485af83d88d1541cc
Andrei Drexler <makro@rq3.com>
2020-12-29 19:48:43 +0200

    Moved compile_shaders.bat to from root to data.

--- data/compile_shaders.bat
+++ data/compile_shaders.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal EnableDelayedExpansion
-pushd %~dp0
+pushd %~dp0..
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 set src_folder=data/shaders
 set out_folder=src/demo/cooked
 set out_file=%out_folder%/cooked_shaders.h
-set compiler=%~dp0output/Win32/Release/shader_compiler/shader_compiler.exe
+set compiler=%~dp0../output/Win32/Release/shader_compiler/shader_compiler.exe
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -18,7 +18,7 @@ for /f "usebackq delims=" %%a in (`echo %cmdcmdline% ^| find /i /c /v "%~dpn0"`)
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 call src\scripts\setup_env.bat
-if NOT ERRORLEVEL 1 msbuild /nologo /v:quiet src\tools\shader_compiler\shader_compiler.vcxproj /p:Configuration=Release /p:Platform=Win32 /p:SolutionDir=%~dp0
+if NOT ERRORLEVEL 1 msbuild /nologo /v:quiet src\tools\shader_compiler\shader_compiler.vcxproj /p:Configuration=Release /p:Platform=Win32 /p:SolutionDir=%~dp0../
 
 if exist "%compiler%" goto has_compiler
 echo ERROR: Shader compiler not found.
