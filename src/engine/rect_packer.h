ac24e0cfb2c3d866d6fc25330b8cc1814309bbc9
Andrei Drexler <makro@rq3.com>
2021-06-24 16:43:11 +0300

    Replaced rect packing algorithm with a simpler one (-88B).

--- src/engine/rect_packer.h
+++ src/engine/rect_packer.h
@@ -15,7 +15,7 @@ struct alignas(u32) RectPacker {
 
 	void						Init(Dimension width, Dimension height);
 
-	const Rectangle*			Add(Dimension width, Dimension height);
+	const Rectangle*			Add(u32 width, u32 height);
 	Index						GetIndex(const Rectangle* rectangle) const { return rectangle - &m_tiles[0]; }
 	const Rectangle&			GetTile(Index index) const	{ return m_tiles[index]; }
 	Index						GetNumTiles() const			{ return m_numTiles; }
@@ -24,136 +24,69 @@ struct alignas(u32) RectPacker {
 	Dimension					GetHeight() const			{ return m_height; }
 
 private:
-	struct Node {
-		using Field				= Dimension;
-
-		Field					flags;
-		Field					data;
-
-		static const Field		Empty = -1;
-		static const Field		AxisBits = 2;
-		static const Field		AxisMask = (1 << AxisBits) - 1;
-		static const Field		LeafTag = AxisMask;
-		static const Field		MaxIndex = Field(-1) >> AxisBits;
-
-		Field					GetAxis() const				{ return flags & AxisMask; }
-		bool					IsLeaf() const				{ return (flags & AxisMask) == LeafTag; }
-		Field					GetNodeSplitPos() const		{ return data; }
-		Field					GetNodeFirstChild() const	{ return flags >> AxisBits; }
-		Field					IsLeafEmpty() const			{ return data == Empty; }
-		Field					GetLeafImageIndex() const	{ return data; }
-
-		void					MakeLeaf(Field data = Empty)								{ this->flags = LeafTag; this->data = data; }
-		void					MakeInternal(bool axis, Field split_pos, Field child)		{ this->flags = Field((child << AxisBits) | Field(axis)); this->data = split_pos; }
-	};
-
-	bool						DoAdd(u16 node_index, Rectangle& node_rect, const Dimension wanted[2]);
-
 	enum {
 		MaxTiles				= 8192,
-		MaxNodes				= 16384,
+		MaxHeight				= 2048,
 	};
 
 	Dimension					m_width;
 	Dimension					m_height;
-	u16							m_numNodes;
-	u16							m_numTiles;
-	Array<Node, MaxNodes>		m_nodes;
+	u32							m_numTiles;
+	Array<int, MaxHeight>		m_rowSize;
 	Array<Rectangle, MaxTiles>	m_tiles;
 };
 
 ////////////////////////////////////////////////////////////////
 
-void RectPacker::Init(Dimension width, Dimension height) {
-	m_width = width;
-	m_height = height;
-	m_numNodes = 1;
-	m_numTiles = 0;
-	m_nodes[0].MakeLeaf();
-}
+FORCEINLINE void RectPacker::Init(Dimension width, Dimension height) {
+	assert(height <= MaxHeight);
 
-FORCEINLINE bool RectPacker::DoAdd(u16 node_index, Rectangle& node_rect, const Dimension wanted[2]) {
-	struct StackEntry {
-		u16 node_index;
-		Rectangle node_rect;
-	};
+	MemSet(this);
 
-	const u16 MaxDepth = 128;
-	StackEntry stack[MaxDepth];
-	u16 stack_top = 0;
-
-	goto beginning;
-
-pop:
-	if (stack_top == 0) {
-		return false;
+	if constexpr (sizeof(m_width) == sizeof(u16)) {
+		// Set both fields with a single instruction.
+		// This saves a tiny bit of code since the function is forceinlined
+		// and the arguments are constants.
+		*(u32*) &m_width = width | (height << 16);
 	} else {
-		auto& top = stack[--stack_top];
-		node_index = top.node_index;
-		node_rect = top.node_rect;
+		m_width = width;
+		m_height = height;
 	}
-
-beginning:
-	assert(node_index < m_numNodes);
-
-	auto& node = m_nodes[node_index];
-	auto axis = node.GetAxis();
-
-	if (axis == Node::LeafTag) {
-		if (!node.IsLeafEmpty())
-			goto pop;
-
-		u32 width = node_rect.GetWidth();
-		u32 height = node_rect.GetHeight();
-		u32 delta_x = width - wanted[0];
-		u32 delta_y = height - wanted[1];
-		i32 delta_xy = delta_x | delta_y;
-
-		if (delta_xy < 0) // width < wanted[0] || height < wanted[1]
-			goto pop;
-
-		if (delta_xy == 0) { // width == wanted[0] && height == wanted[1]
-			m_nodes[node_index].data = m_numTiles;
-			m_tiles[m_numTiles++] = node_rect;
-			assert(m_numTiles <= size(m_tiles));
-			return true;
-		}
-
-		axis = delta_y > delta_x;
-		Node::Field split_pos = node_rect.min[axis] + wanted[axis];
-
-		auto first_child = (Node::Field) m_numNodes;
-		m_nodes[first_child + 0].MakeLeaf();
-		m_nodes[first_child + 1].MakeLeaf();
-		m_numNodes += 2;
-		assert(m_numNodes <= size(m_nodes));
-
-		node.MakeInternal(axis, split_pos, first_child);
-	}
-
-	auto split_pos = node.GetNodeSplitPos();
-	auto first_child = node.GetNodeFirstChild();
-
-	assert(stack_top < MaxDepth);
-	auto& push = stack[stack_top++];
-	push.node_index = first_child + 1;
-	MemCopy(&push.node_rect, &node_rect);
-	push.node_rect.min[axis] = split_pos;
-
-	node_rect.max[axis] = split_pos;
-	node_index = first_child;
-
-	goto beginning;
 }
 
-NOINLINE const RectPacker::Rectangle* RectPacker::Add(Dimension width, Dimension height) {
-	assert(m_numTiles < MaxTiles);
+NOINLINE const RectPacker::Rectangle* RectPacker::Add(u32 width, u32 height) {
+	int best_y = -1;
+	int best_x = m_width;
 
-	Dimension wanted[2] = {width, height};
-	Rectangle full{{0, 0}, {m_width, m_height}};
+	for (int test_y = 0, avail_height = m_height - height; test_y < avail_height; ++test_y) {
+		int max_x = 0;
+		for (int y = 0; y < height; ++y) {
+			int current = m_rowSize[test_y + y];
+			if (max_x < current)
+				max_x = current;
+		}
+		max_x += width;
 
-	if (!DoAdd(0, full, wanted))
+		if (max_x < best_x) {
+			best_x = max_x;
+			best_y = test_y;
+		}
+	}
+
+	if (best_y < 0)
 		Sys::Fatal(Error::Atlas);
 
-	return &m_tiles[m_numTiles - 1];
+	MemSet32(&m_rowSize[best_y], best_x, height);
+
+	Rectangle& rect = m_tiles[m_numTiles++];
+
+	rect.max[0] = best_x;
+	best_x -= width;
+	rect.min[0] = best_x;
+
+	rect.min[1] = best_y;
+	best_y += height;
+	rect.max[1] = best_y;
+
+	return &rect;
 }
