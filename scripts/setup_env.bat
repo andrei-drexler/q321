ced9197508a72297da1fdf71987ad58183843d1c
Andrei Drexler <makro@rq3.com>
2021-07-28 22:43:13 +0300

    Use Windows SDK 8.1 (-31B).

--- scripts/setup_env.bat
+++ scripts/setup_env.bat
@@ -1,9 +1,6 @@
 @echo off
 
 set check_cl=where cl.exe ^>nul 2^>nul
-%check_cl% && exit /b 0
-
-:: Not running from a VS Developer Command Prompt, need to set up environment manually
 
 :: VS2017 or newer
 set vs2017_where="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -11,7 +8,7 @@ if exist %vs2017_where% goto try_vs2017
 goto no_env
 
 :try_vs2017
-for /f "usebackq delims=#" %%a in (`%vs2017_where% -latest -property installationPath`) do call "%%a\Common7\Tools\VsDevCmd.bat" >nul
+for /f "usebackq delims=#" %%a in (`%vs2017_where% -latest -property installationPath`) do call "%%a\Common7\Tools\VsDevCmd.bat" -arch=x86 -winsdk=8.1 -vcvars_ver=14.1 >nul
 
 %check_cl% && exit /b 0
 
